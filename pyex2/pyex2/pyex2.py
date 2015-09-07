cd6eb5cd1752db276f175f9c159d5a37af250d03
dmoon4117 <dmoon4117@naver.com>
2015-09-07 22:03:11 +0900

    복습
    1

---
+++ pyex2/pyex2/pyex2.py
@@ -0,0 +1,32 @@
+#print("Here are the number!"+"The first is {0:d}The second is {1:4d}".format(7,8)
+#input이 String을 반환하기때문에 float로 형변환하여 호출
+
+
+#print("김동현")
+#한글을 쓰려면 고급저장옵션으로 가서 유니코드 utf-8로 변경
+
+#salary = input("Please enter your salary:")
+#bonus = input("Please enter your bonus:")
+#payCheck = salary+bonus
+#payCheck = float(salary)+float(bonus)
+#print(payCheck)
+#print(type(payCheck)) //타입체크하는함수 print(type(@))
+
+#unexpected EOR 에러 뜨면 ()확인해볼것
+
+#print(round(3.67552468756,3))
+#round(num,d) num을 소수점 d자리까지 반올림
+
+#name = '동해물과백두산이하느님이보우하사'
+#print(name[0])
+#print(name[-1])
+#print(name[-2])
+#print(name[-3])
+#0은 처음 -1은 끝 -2, -3은 끝에서 역순
+
+#info = "201111262김동현"
+#sid = info[:9]
+#sname = info[9:]
+#print(sid)
+#print(sname)
+ 
