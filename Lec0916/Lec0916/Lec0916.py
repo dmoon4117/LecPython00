270af42d5dfdbf7d011fd5156d114c82ae4d3f8e
dmoon4117 <dmoon4117@naver.com>
2015-09-22 20:02:17 +0900

    20150916_20150921

---
+++ Lec0916/Lec0916/Lec0916.py
@@ -0,0 +1,88 @@
+﻿#fileName = "greenjoa.txt."
+#WRITE = "w"
+#myFile = open(fileName, "w")
+#myFile.write("hi")
+
+#myFile.close() #파일을 열었으면 꼭 닫아줘야한다.
+
+#fileName = "text.txt"
+#READ = "r"
+#myFile = open(fileName, mode = READ)
+#cotent = myFile.readlines()
+#for line in content:
+#    print(line)
+#myFile.close()
+
+ 
+#fileName = "donghyun.txt"
+#with open(fileName, "w+") as myFile :
+#    myFile.write("201111262 김동현")
+#    myFile.write("201111261 곽민기")
+
+
+
+#fileName = "donghyun.txt"
+
+#with open(fileName, "r") as myFile :
+#    content = myFile.read() 
+#    print(content)  
+
+#fileName = "파일입출력예제1.txt"
+
+#with open(fileName, "r") as myFile :
+#    content = myFile.read() 
+#    print(content)  
+  
+
+#fileName = "파일입출력예제1.txt"
+
+#with open(fileName, "r") as myFile :
+#    for content in myFile : #줄단위로
+#      print(content)  
+
+#fileName = "파일입출력예제1.txt"
+#fileName2 = "Monica.txt"
+
+#with open(fileName, "r") as myFile, open(fileName2, "w") as monica :
+#    for content in myFile : #줄단위로
+#        (role, etc) = content.strip().split(":") # :를 기준으로 자른다 
+#        if ( role == "Monica") :                 #strip함수
+#            monica.write(etc)
+#            monica.write("\n") #줄바꿈
+
+#fileName = "파일입출력예제2.txt"
+#fileName2 = "Monica.txt"
+
+
+#with open(fileName, "r") as myFile, open(fileName2, "w") as monica :
+#    for content in myFile :  
+#        (role, etc) = content.strip().split(":",1)  
+#        if ( role == "Monica") :                  
+#            monica.write(etc)
+#            monica.write("\n")  
+
+
+#fileName = "파일입출력예제2.txt"
+#fileName2 = "Monica.txt"
+#roles = [] #빈리스트
+
+#with open(fileName, "r") as myFile, open(fileName2, "w") as monica :
+#    for content in myFile :  
+#        (role, etc) = content.strip().split(":",1)  
+#        roles.append(role) #리스트에 role를 추가
+#print(roles)
+ 
+#import pickle #두가지함수를 통해 파일을 저장하고 다시 불러올수 있다.
+#fileName = "파일입출력예제2.txt"
+#fileName2 = "Monica.txt"
+#roles = []  
+
+#with open(fileName, "r") as myFile, open(fileName2, "wb") as monica : #pickle 바이너리형태
+#    for content in myFile :  
+#        (role, etc) = content.strip().split(":",1)  
+#        roles.append(role)
+#    pickle.dump(roles, monica)
+  
+#with open(fileName2, "rb") as monica :
+#    result = pickle.load(monica)
+#    print(result)
---
+++ Lec0921/Lec0921/Lec0921.py
@@ -0,0 +1,153 @@
+﻿#class Service:
+#    secret = '영구는 배꼽이 두 개다'
+#    def sum(self, a, b):
+#        result = a + b
+#        print('%s + %s = %s 입니다.' % (a, b, result))
+
+#pey = Service()
+#print(pey.secret)
+
+#pey.sum(1,1) 
+#Service.sum(pey,1,1)
+
+#class movie:
+#    title = "베테랑"
+#    director = "류승완"
+#    def __inif__(self, title, director):
+#        self.title = title
+#        self.director = director
+
+#    def showinfo(self):
+#        print(self.title + " " + self.director)
+#show = movie("베테랑","류승완")
+#show2 = movie()
+
+#Movie.personal #perosnal 이란 속성이 없으므로 오류 
+
+#class movie: 
+#    '''영화 클래스입니다.'''
+#    title = "스타워즈"
+#    director = "조지 루카스"
+#    def __init__(self, title, director):
+#        self.title = title
+#        self.director = director
+#        print(self.title + " 생성자 호출 ")
+    
+#    def __del__(self):
+#        print(self.title + " 소멸자 호출 ")
+
+#    def showinfo(self):
+#        print(self.title + " " + self.director)
+#m1 = movie("베테랑1","류승완1");
+#m2 = movie("베테랑2","류승완2");
+#m3 = movie("베테랑3","류승완3");
+
+
+
+#class movie: 
+#    '''영화 클래스입니다.'''
+#    title = "스타워즈"
+#    director = "조지 루카스"
+#    def __init__(self, title, director):
+#        self.title = title
+#        self.director = director
+#        print(self.title + " 생성자 호출 ")
+    
+#    def __del__(self):
+#        print(self.title + " 소멸자 호출 ")
+
+#    def showinfo(self):
+#        print(self.title + " " + self.director)
+#m1 = movie("베테랑1","류승완1");
+#m2 = movie("베테랑2","류승완2");
+#m3 = movie("베테랑3","류승완3");
+#m4 = movie("베테랑4","류승완4");
+#print(type(m4))
+#m4 = m3
+#m4.showinfo()
+#파이썬도 참조형 대입연산을 하면 똑같은 주소를 가리키게 된다
+
+#class movie: 
+#    '''영화 클래스입니다.'''
+#    title = "스타워즈"
+#    director = "조지 루카스"
+#    def __init__(self, title, director):
+#        self.title = title
+#        self.director = director
+#        print(self.title + " 생성자 호출 ")
+    
+#    def __del__(self):
+#        print(self.title + " 소멸자 호출 ")
+
+#    def showinfo(self):
+#        print(self.title + " " + self.director)
+#m1 = movie("베테랑1","류승완1");
+#m2 = movie("베테랑2","류승완2");
+#m3 = movie("베테랑3","류승완3");
+#m4 = movie("베테랑4","류승완4");
+#print(type(m4))
+#m4 = m3
+#print(id(m4))
+#print(id(m3))
+#m4.showinfo()
+
+#class movie: 
+#    '''영화 클래스입니다.'''
+#    title = "스타워즈"
+#    director = "조지 루카스"
+#    def __init__(self, title, director):
+#        self.title = title
+#        self.director = director
+#        print(self.title + " 생성자 호출 ")
+    
+#    def __del__(self):
+#        print(self.title + " 소멸자 호출 ")
+
+#    def showinfo(self):
+#        print(self.title + " " + self.director)
+    
+         
+#m1 = movie("베테랑1","류승완1");
+#m2 = movie("베테랑2","류승완2");
+#m3 = movie("베테랑3","류승완3");
+#m4 = movie("베테랑4","류승완4");
+#print(type(m4))
+#m4 = m3
+#print(id(m4))
+#print(id(m3))
+#m4.showinfo()
+
+class movie: 
+    '''영화 클래스입니다.'''
+    count = 0  
+    title = "스타워즈"
+    director = "조지 루카스"
+    def __init__(self, title, director):
+        self.title = title
+        self.director = director
+        movie.count+=1 
+        print(self.title + " 생성자 호출 ")
+    
+    def __del__(self):
+        print(self.title + " 소멸자 호출 ")
+
+    def showinfo(self):
+        print(self.title + " " + self.director)
+
+    @staticmethod 
+    def showcount():
+        print(movie.count)
+
+    @classmethod 
+    def showcount2(cls):
+        print(cls.count)
+ 
+m1 = movie("a","b")
+m2 = movie("c","d")
+m3 = movie("c","d")
+m3 = movie("c","d")
+m4 = movie("c","d")
+m5 = movie("c","d")
+
+movie.showcount()
+movie.showcount2()
